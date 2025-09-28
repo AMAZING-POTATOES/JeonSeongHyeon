75ee638f9dae089c3132a30a60b31630305c2978
whiteblack1858 <jsh147301@gmail.com>
2025-09-29 01:52:04 +0900

    MODIFIED : repository findBySubject() method in testApplication

--- src/main/java/com/example/demo/QuestionRepository.java
+++ src/main/java/com/example/demo/QuestionRepository.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository<Question, Integer>{
-	
-
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+	Question findBySubject(String subject);
 }
