7d9eaf282ef868877420722dee30f7ec578fb2da
whiteblack1858 <jsh147301@gmail.com>
2025-09-29 02:26:35 +0900

    MODIFIED : save the answer data in testApplication

--- src/test/java/com/example/demo/SbbApplicationTests.java
+++ src/test/java/com/example/demo/SbbApplicationTests.java
@@ -1,8 +1,9 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals; // 현재 예제에서는 사용되지 않고 있습니다.
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -15,14 +16,20 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 	
+	@Autowired
+	private AnswerRepository answerRepository;
+	
 	@Test
 	void testJpa() {
-		assertEquals(2, this.questionRepository.count());
-		Optional<Question> oq = this.questionRepository.findById(1);
+		Optional<Question> oq = this.questionRepository.findById(2);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
-		this.questionRepository.delete(q);
-		assertEquals(1, this.questionRepository.count());
+		
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q);
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
 	}
 
 }
